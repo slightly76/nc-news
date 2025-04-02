addf9f605c45f256992a8c1589ee72e05ae24021
slightly76 <slightly76@keyop.>
2025-04-02 12:09:39 +0100

    task6 added comments section below article, did some tidying

--- src/components/NewArticleList.jsx
+++ src/components/NewArticleList.jsx
@@ -1,64 +1,64 @@
 import React from 'react';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import getDaysPassed from './getDaysPassed';
 import { usePageTitle } from './PageTitleContext';
 import './newArticleList.css';
 
-function NewArticleList({} ) {
+function NewArticleList({}) {
+	const [newArticles, setNewArticles] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const { setPageTitle } = usePageTitle();
 
-    const [newArticles, setNewArticles] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const { setPageTitle } = usePageTitle();
+	useEffect(() => {
+		setIsLoading(true);
+		setPageTitle('Recent Articles');
 
-useEffect (() => {
-    setIsLoading(true);
-    setPageTitle('Recent Articles');
+		Promise.all([
+			axios.get(
+				'https://slightly76-does-nc-news.onrender.com/api/articles?sort_by=created_at'
+			),
+		])
+			.then(([articlesResult]) => {
+				setNewArticles(articlesResult.data.articles);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+				setIsLoading(false);
+			});
+	}, [setPageTitle]);
 
-    Promise.all([
-        axios.get('https://slightly76-does-nc-news.onrender.com/api/articles?sort_by=created_at')
-    ])
-    .then(([response]) => {
-        
-        setNewArticles(response.data.articles);
-        setIsLoading(false);
-    })
-    .catch((error) => {
-        console.error('Error fetching data:', error);
-        setIsLoading(false)
-    });
-}, [setPageTitle]);
+	if (isLoading) return <p className='message'>Please wait, loading...</p>;
 
-if (isLoading) return <p className="message">Please wait, loading...</p>;
+	return (
+		<>
+			<div className='articleListContainer'>
+				{newArticles.map((article) => {
+					const time = getDaysPassed(article.created_at);
+					return (
+						<div className='articleListCard' key={article.article_id}>
+							<Link
+								to={`/articles/${article.article_id}`}
+								className='articleListTitle'
+							>
+								{article.title}
+							</Link>
+							{/* <p className='articleListTitle'>{article.title}</p> */}
+							<p className='articleAuthor'>
+								in {article.topic} by {article.author}
+							</p>
+							{/* <img src={article.author.avatar_url} className="userAvatar" alt="user avatar"></img> */}
 
-return (
-    <>
-   
-    <div className="articleListContainer">
-        
-
-        {newArticles.map((article) => {
-           const time = getDaysPassed(article.created_at);
-           return (
-                <div className="articleListCard" key={article.article_id}>
-                    <Link to={`/articles/${article.article_id}`} className="articleListTitle">
-                    {article.title}
-                    </Link>
-                 {/* <p className="articleTitle">{article.title}</p> */}
-                 <p className="articleAuthor">in {article.topic} by {article.author}</p>
-                 {/* <img src={article.author.avatar_url} className="userAvatar" alt="user avatar"></img> */}
-                 
-                 <p className="articleTimeStamp">{time}</p>
-                 <br></br>
-
-                </div>
-           )
-     })}
-        </div>
-    </>
-
-);
+							<p className='articleTimeStamp'>{time}</p>
+							<br></br>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
 export default NewArticleList;
